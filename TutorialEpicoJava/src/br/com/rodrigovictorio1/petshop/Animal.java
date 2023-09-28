769c1be37f0db8412a5f47ab5adb3550939a34cf
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2023-09-28 17:11:50 -0300

    Outros arquivos

---
+++ TutorialEpicoJava/src/br/com/rodrigovictorio1/petshop/Animal.java
@@ -0,0 +1,9 @@
+package br.com.rodrigovictorio1.petshop;
+
+public class Animal {
+
+	public static String iAmDog() {
+		return "I am a dog";
+	}
+
+}
---
+++ TutorialEpicoJava/src/br/com/rodrigovictorio1/petshop/Main.java
@@ -0,0 +1,11 @@
+package br.com.rodrigovictorio1.petshop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		String dog = a.iAmDog();
+		System.out.println(dog);
+	}
+
+}
