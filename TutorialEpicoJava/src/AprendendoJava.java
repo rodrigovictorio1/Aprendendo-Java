2cac1cd0228235ef682f66df7d52d25609213236
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2023-09-28 15:46:08 -0300

    Adicionada alterações ao projeto.

--- TutorialEpicoJava/src/AprendendoJava.java
+++ TutorialEpicoJava/src/AprendendoJava.java
@@ -2,8 +2,11 @@
 public class AprendendoJava {
 
 	public static void main(String[] args) {
-		
-
+		addExclamationPoint("!");
+	}
+	
+	public static void addExclamationPoint(String s) {
+		System.out.println("Olá, mundo" + s);
 	}
 
 }
